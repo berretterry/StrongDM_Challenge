2a7e2f003849ecafc59a38df9365fec7fec4387b
Berret Terry <berretterry@gmail.com>
2024-08-21 15:19:35 -0700

    trying to get rid of the .pem file from the commit

--- Infrastructure/security_groups.tf
+++ Infrastructure/security_groups.tf
@@ -79,9 +79,17 @@ resource "aws_vpc_security_group_egress_rule" "app_egress" {
   ip_protocol    = "-1"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "app_ssh" {
+resource "aws_vpc_security_group_ingress_rule" "jump_ssh" {
   security_group_id = aws_security_group.app_sg.id
   referenced_security_group_id = aws_security_group.jump_sg.id
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_ssh" {
+  security_group_id = aws_security_group.app_sg.id
+  cidr_ipv4 = var.app_subnet_cidr
   from_port = 22
   ip_protocol = "tcp"
   to_port = 22
@@ -160,6 +168,14 @@ resource "aws_vpc_security_group_ingress_rule" "db_app_web" {
 resource "aws_vpc_security_group_ingress_rule" "db_app" {
   security_group_id = aws_security_group.db_sg.id
   referenced_security_group_id = aws_security_group.app_sg.id
+  from_port = 3306
+  ip_protocol = "tcp"
+  to_port = 3306
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_relay" {
+  security_group_id = aws_security_group.db_sg.id
+  cidr_ipv4 = var.db_subnet_cidr[0]
   from_port = 3306
   ip_protocol = "tcp"
   to_port = 3306
